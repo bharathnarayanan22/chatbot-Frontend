a8c3a5c1675b41895426bc5301f850f639dcca08
bharathnarayanan22 <123437201+bharathnarayanan22@users.noreply.github.com>
2024-04-13 15:11:20 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -34,8 +34,8 @@ const Login = () => {
     return (
         <Box width={'100%'} height={'100%'} display={'flex'} flex={1}>
             <Box padding={8} mt={8} display={{ md: "flex"}}>
-                {/* <img src="airobot.png" alt="Robot" style={{ width: "400px" }} /> */}
-                <Spline scene="https://prod.spline.design/8koAhSiaurIdL1Vr/scene.splinecode" />
+                <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
+{/*                 <Spline scene="https://prod.spline.design/8koAhSiaurIdL1Vr/scene.splinecode" /> */}
             </Box>
             <Box
                 display={"flex"}
